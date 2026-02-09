ed6439090457cfc36e1b5772175da69b03a44f46
hijodelhombre <hijodelhombre33@gmail.com>
2026-02-09 15:46:39 +0000

    Fix Android Gradle config for Flutter

--- pads_sampler/android/build.gradle.kts
+++ pads_sampler/android/build.gradle.kts
@@ -5,26 +5,6 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-ext {
-    compileSdkVersion = 36
-    targetSdkVersion = 36
-    minSdkVersion = 21
-    buildToolsVersion = "36.0.0"
+    delete(rootProject.buildDir)
 }
